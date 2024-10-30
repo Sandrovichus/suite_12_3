656f9da053aa5ea29c9e68d11f5aa2bd0e3aecb7
Alexander <veber21@rambler.ru>
2024-10-30 13:08:10 +0300

    Add files via upload

---
+++ tests_12_3.py
@@ -0,0 +1,87 @@
+import unittest
+
+from runner import Runner
+from runner_and_tournament import Runner, Tournament
+
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_walk(self):
+        t_runner = Runner('Name')
+        for i in range(10):
+            t_runner.walk()
+        self.assertEqual(t_runner.distance, 50)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run(self):
+        t_runner = Runner('Name')
+        for i in range(10):
+            t_runner.run()
+        self.assertEqual(t_runner.distance, 100)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_challenge(self):
+        t_runner_1 = Runner('Name_1')
+        t_runner_2 = Runner('Name_2')
+        for i in range(10):
+            t_runner_1.run()
+            t_runner_2.walk()
+        self.assertNotEqual(t_runner_1.distance, t_runner_2.distance)
+
+
+class TournamentTest(unittest.TestCase):
+    is_frozen = True
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.runner_1 = Runner('Усэйн', 10)
+        self.runner_2 = Runner('Андрей', 9)
+        self.runner_3 = Runner('Ник', 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for i in cls.all_results:
+            temp_dict = {}
+            for j in cls.all_results[i]:
+                temp_dict[j] = str(cls.all_results[i][j])
+            print(temp_dict)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run_1(self):
+        tournament1 = Tournament(90, self.runner_1, self.runner_3)
+        self.all_results[1] = tournament1.start()
+        self.assertTrue(self.all_results[1][max(self.all_results[1].keys())] == 'Ник')
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run_2(self):
+        tournament2 = Tournament(90, self.runner_2, self.runner_3)
+        self.all_results[2] = tournament2.start()
+        self.assertTrue(self.all_results[2][max(self.all_results[2].keys())] == 'Ник')
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run_3(self):
+        tournament3 = Tournament(90, self.runner_1, self.runner_2, self.runner_3)
+        self.all_results[3] = tournament3.start()
+        self.assertTrue(self.all_results[3][max(self.all_results[3].keys())] == 'Ник')
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    # дополнительный тесты для проверки, чтобы медленные бегуны не оказались на первых местах при маленьких дистанциях
+    def test_run_4(self):
+        tournament3 = Tournament(6, self.runner_3, self.runner_2, self.runner_1)
+        self.all_results[3] = tournament3.start()
+        self.assertTrue(self.all_results[3][max(self.all_results[3].keys())] == 'Ник')
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run_5(self):
+        tournament3 = Tournament(1, self.runner_3, self.runner_2, self.runner_1)
+        self.all_results[3] = tournament3.start()
+        self.assertTrue(self.all_results[3][max(self.all_results[3].keys())] == 'Ник')
+
+
+if __name__ == '__main__':
+    unittest.main()
